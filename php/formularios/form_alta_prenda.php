63df56030b18ea54cd0ca1db5b5959f3c711d85e
Miguel Macarro <acc@miguelmacarro.es>
2017-06-05 18:18:43 +0200

    empezado con validación php de form_alta_prenda, fallos en js y al ejecutar insert

--- php/formularios/form_alta_prenda.php
+++ php/formularios/form_alta_prenda.php
@@ -13,40 +13,38 @@ cerrarConexionBD($conexion);
 
 <h2> Introduce los datos de la prenda: </h2>
 <div id="divFormAltaPrenda">
-	<form id="formAltaPrenda" enctype="multipart/form-data" action='php/controladores/insert.php' method="post" onSubmit="return validateForm()">
-		<label> Color: </label><br>
+	<form id="formAltaPrenda" class="altas-form" enctype="multipart/form-data" action='php/controladores/insert.php' method="post" onSubmit="return validateForm()">
+		<label>Color:</label><br>
 			<input type="text" name="colorPrenda" id="colorPrenda" onBlur="colorValidation()"><br>
-		<label> Tipo: </label><br>
+		<label>Tipo:</label><br>
 			<div id="divRadio" name="divRadio">
-				<p> Camiseta - Sudadera - HeadWear </p><br>
-				<input type="radio" name="tipoPrenda" id="tipoPrenda" value="Camiseta" checked>
-				<input type="radio" name="tipoPrenda" id="tipoPrenda" value="Sudadera">
-				<input type="radio" name="tipoPrenda" id="tipoPrenda" value="Headwear">
+				<input type="radio" name="tipoPrenda" id="tipoPrenda" value="Camiseta" checked>Camiseta<br>
+				<input type="radio" name="tipoPrenda" id="tipoPrenda" value="Sudadera"> Sudadera<br>
+				<input type="radio" name="tipoPrenda" id="tipoPrenda" value="Headwear"> Headwear
 			</div>
 		<label> Calidad: </label><br>
-			<input type="number" name="calidadPrenda" id="calidadPrenda" onBlur="calidadValidation()"><br>
+			<input type="number" min="0" max="10" name="calidadPrenda" id="calidadPrenda" onBlur="calidadValidation()"><br>
 		<label> Talla: </label><br>
 			<div id="divRadio" name="divRadio">
-				<p> XXL - XL - L - M - S </p><br>
-				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="XXL">
-				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="XL">
-				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="L" checked>
-				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="M">
-				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="S">
+				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="S"> S<br>
+				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="M"> M<br>
+				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="L" checked> L<br>
+				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="XL"> XL<br>
+				<input type="radio" name="tallaPrenda" id="tipoPrenda" value="XXL"> XXL	
 			</div>
 		<label> Precio: </label><br>
-			<input type="number" name="precioPrenda" id="precioPrenda" onBlur="precioValidation()"><br>
+			<input type="number" min="0" name="precioPrenda" id="precioPrenda" onBlur="precioValidation()"><br>
 		<label> Añade una imagen: </label><br>
 			<input type="file" name="imagenPrenda" id="imagenPrenda" accept="image/*"><br>
 		<label> Cantidad: </label><br>
-			<input type="number" name="cantidadPrenda" id="cantidadPrenda" onBlur="cantidadValidation()"><br>
+			<input type="number" min="0" name="cantidadPrenda" id="cantidadPrenda" onBlur="cantidadValidation()"><br>
 		<label> ¿Pertenece a alguna de éstas temporadas? </label><br>
 			<select name="selectTemporadaPrenda" id="selectTemporadaPrenda"><br>
 				<option value="null">No</option>
 				<?php foreach($temporada as $fila){?>
 				<option value="<?php echo $fila["NOMBRETEMPORADA"]; ?>"><?php echo $fila["NOMBRETEMPORADA"]; ?> </option>
 				<?php }?>
-			</select>
+			</select><br>
 		<label> ¿Es de alguno de éstos proveedores? </label><br>
 			<select name="selectProveedorPrenda" id="selectProveedorPrenda"><br>
 				<option value="null">No</option>
@@ -60,7 +58,7 @@ cerrarConexionBD($conexion);
 				<?php foreach($colaboradores as $fila){?>
 				<option value="<?php echo $fila["NOMBRECOLABORADORTEXTIL"]; ?>"><?php echo $fila["NOMBRECOLABORADORTEXTIL"]; ?> </option>
 				<?php }?>
-			</select><br>
+			</select><br><br>
 		<button type="submit" id="botonSubirPrenda" name="botonSubirPrenda">Enviar</button>
 	</form>
 </div>
